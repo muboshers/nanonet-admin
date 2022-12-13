1d526e9d412aafae15dd704ac85a20480195c47d
Mubosher Muydinov <muboshermuydinov5@gmail.com>
2022-12-13 12:32:42 +0500

    dedewdewdwed

---
+++ dist/_nuxt/static/1670916690/ru/contact/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1670916690",layout:"default",error:a,state:{admin:a,i18n:{routeParams:{}}},serverRendered:true,routePath:"\u002Fru\u002Fcontact",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
